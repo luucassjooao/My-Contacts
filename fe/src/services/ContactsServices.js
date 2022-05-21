c0df8ef0d80f50c866f14a7ec7d1cdfaba3484b6
lucasjoaoo <mainexcalibur@protonmail.com>
2022-05-21 20:47:38 -0300

    refactored HttpClient

--- fe/src/services/ContactsServices.js
+++ fe/src/services/ContactsServices.js
@@ -10,7 +10,7 @@ class ConctactService {
   }
 
   async createContacts(contact) {
-    return this.httpClient.post('/contacts', contact);
+    return this.httpClient.post('/contacts', { body: contact });
   }
 }
 
