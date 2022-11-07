451b9749dd936dec75e2a530251b2331864565f7
lucasjoaoo <mainexcalibur@protonmail.com>
2022-11-07 19:20:23 -0300

    abstraindo logica de animação para um custom hook

--- fe/src/components/Loader/index.js
+++ fe/src/components/Loader/index.js
@@ -3,15 +3,18 @@ import { Overlay } from './styles';
 import Spinner from '../Spinner';
 
 import ReactPortal from '../ReactPortal';
+import useAnimatedUnmount from '../../hooks/useAnimatedUnmount';
 
 export default function Loader({ isLoading }) {
-  if (!isLoading) {
+  const { shouldRender, animatedElementRef } = useAnimatedUnmount(isLoading);
+
+  if (!shouldRender) {
     return null;
   }
 
   return (
     <ReactPortal containerId="loader-root">
-      <Overlay>
+      <Overlay ref={animatedElementRef} isLeaving={!isLoading}>
         <Spinner size={90} />
       </Overlay>
     </ReactPortal>
